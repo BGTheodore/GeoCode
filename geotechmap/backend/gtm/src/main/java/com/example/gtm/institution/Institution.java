6ddb21d80fa396f83dcb3a8ac6d12b48e96f22d2
Barbara <bgtheodore18@gmail.com>
2021-04-25 21:19:54 -0400

    Institution

--- geotechmap/backend/gtm/src/main/java/com/example/gtm/institution/Institution.java
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/institution/Institution.java
@@ -17,4 +17,73 @@ public class Institution {
     private String isDeleted;
     private String updatedBy;
     private String deletedBy;
+
+    public Institution() {
+    }
+
+    public Institution( Long id, 
+                        String name, 
+                        String address, 
+                        String phone1, 
+                        String phone2, 
+                        String email, 
+                        String website,
+                        String taxNumber, 
+                        String description, 
+                        String createdAt, 
+                        String updatedAt, 
+                        String deletedAt,
+                        String createdBy, 
+                        String isDeleted, 
+                        String updatedBy, 
+                        String deletedBy) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.website = website;
+        this.taxNumber = taxNumber;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.createdBy = createdBy;
+        this.isDeleted = isDeleted;
+        this.updatedBy = updatedBy;
+        this.deletedBy = deletedBy;
+    }
+    
+    public Institution( String name, 
+                        String address, 
+                        String phone1, 
+                        String phone2, 
+                        String email, 
+                        String website,
+                        String taxNumber, 
+                        String description, 
+                        String createdAt, 
+                        String updatedAt, 
+                        String deletedAt,
+                        String createdBy, 
+                        String isDeleted, 
+                        String updatedBy, 
+                        String deletedBy) {
+        this.name = name;
+        this.address = address;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.website = website;
+        this.taxNumber = taxNumber;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.createdBy = createdBy;
+        this.isDeleted = isDeleted;
+        this.updatedBy = updatedBy;
+        this.deletedBy = deletedBy;
+    }
 }
