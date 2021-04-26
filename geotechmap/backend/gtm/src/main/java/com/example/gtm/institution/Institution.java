e90e028bacd38ca00478d01ed11b8a6eb5ce1187
Barbara <bgtheodore18@gmail.com>
2021-04-25 21:05:47 -0400

    Institution

---
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/institution/Institution.java
@@ -0,0 +1,20 @@
+package com.example.gtm.institution;
+
+public class Institution {
+    private Long id;
+    private String name;
+    private String address;
+    private String phone1;
+    private String phone2;
+    private String email;
+    private String website;
+    private String taxNumber;
+    private String description;
+    private String createdAt;
+    private String updatedAt;
+    private String deletedAt;
+    private String createdBy;
+    private String isDeleted;
+    private String updatedBy;
+    private String deletedBy;
+}
