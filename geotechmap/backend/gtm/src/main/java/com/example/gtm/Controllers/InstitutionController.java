a927c64f0fac9efa5855ac3e001956206f7b7ede
Barbara <bgtheodore18@gmail.com>
2021-05-04 22:26:26 -0400

    Adding Institution

--- geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/InstitutionController.java
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/InstitutionController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,9 @@ public class InstitutionController {
         return ResponseEntity.ok().body(service.updateInstitution(id, institution));
     }
 
-
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteInstitution(@PathVariable Long id){
+        service.deleteInstitution(id);
+    }
 }
