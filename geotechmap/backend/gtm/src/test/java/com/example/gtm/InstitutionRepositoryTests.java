10f60388cfaf7177151ddd048452d2e9cd03954e
Barbara <bgtheodore18@gmail.com>
2021-05-04 22:51:32 -0400

    Adding Institution

--- geotechmap/backend/gtm/src/test/java/com/example/gtm/InstitutionRepositoryTests.java
+++ geotechmap/backend/gtm/src/test/java/com/example/gtm/InstitutionRepositoryTests.java
@@ -1,5 +1,11 @@
 package com.example.gtm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.example.gtm.Entities.Institution;
 import com.example.gtm.Repositories.InstitutionRepository;
 import com.example.gtm.Services.InstitutionService;
@@ -22,7 +28,10 @@ class InstitutionRepositoryTests {
     @Test
     public void getInstitutionTest(){
         Institution institution = new Institution();
-        institution.setName("URGeo");
+        institution.setName("Unité de Recherche en Géosciences");
+
+        when(repository.findAll()).thenReturn(Stream.of(institution).collect(Collectors.toList()));
+        assertEquals(1, service.listAllInstitutions().size());
     }
 
 }
