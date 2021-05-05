b1a33ef47e719ae4ffebc3b8830907956dd52a38
Barbara <bgtheodore18@gmail.com>
2021-05-04 21:35:52 -0400

    Adding Institution

--- geotechmap/backend/gtm/src/main/java/com/example/gtm/Services/InstitutionService.java
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Services/InstitutionService.java
@@ -1,5 +1,10 @@
 package com.example.gtm.Services;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.gtm.Entities.Institution;
+import com.example.gtm.Exception.ResourceNotFoundException;
 import com.example.gtm.Repositories.InstitutionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,5 +15,21 @@ public class InstitutionService {
     @Autowired
     InstitutionRepository repository;
 
-    
+    public Institution createNewInstitution(Institution institution) {
+        return repository.save(institution);
+    }
+
+    public List<Institution> listAllInstitutions(){
+        return repository.findAll();
+    }
+
+    public Institution updateInstitution(Long id, Institution institution){
+        Optional<Institution> optional = repository.findById(id);
+        if (!optional.isPresent()){
+            throw new ResourceNotFoundException("Institution not found with id :" + id );
+        } else {
+            institution.setId(id);
+            return repository.save(institution);
+        }
+    }
 }
