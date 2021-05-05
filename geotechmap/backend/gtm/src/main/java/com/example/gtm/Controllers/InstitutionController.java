5c309aca390f74b1301b7f1201f127f40dff73b2
Barbara <bgtheodore18@gmail.com>
2021-05-04 21:46:03 -0400

    Adding Institution

---
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/InstitutionController.java
@@ -0,0 +1,10 @@
+package com.example.gtm.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/institutions")
+public class InstitutionController {
+    
+}
