76221a16be5fd63042cc4922193e51d077be5fc2
Barbara <bgtheodore18@gmail.com>
2021-05-16 20:37:47 -0400

    Frontend essai

--- geotechmap/backend/frontend/src/views/essais/typeEssai.js
+++ geotechmap/backend/frontend/src/views/essais/typeEssai.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react'
 
-const TypeEssai =  => {
+const TypeEssai = (props) => {
     return (
         <CRow>
           <CCol lg={6}>
@@ -11,21 +11,7 @@ const TypeEssai =  => {
               </CCardHeader>
               <CCardBody>
                 <ul>
-                  <li>Nom : {props.institution.nom}</li>
-                  <li>Sigle : {propimport React from 'react'
-import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react'
-
-const Essai =  => {
-    return (
-        <CRow>
-          <CCol lg={6}>
-            <CCard>
-              <CCardHeader  className="text-muted">
-               Identifiant de l'essai : {props.institution.id}
-              </CCardHeader>
-              <CCardBody>
-                <ul>
-                  <li>Nom : {props.institution.nom}</li>
+                  <li>Nom : {props.typeEssai.nom}</li>
                   <li>Sigle : {props.institution.sigle}</li>
                   <li>Site web : {props.institution.siteWeb}</li>
                   <li>Email : {props.institution.email}</li>
@@ -46,24 +32,4 @@ const Essai =  => {
       )
     }
     
-    export default Essais.institution.sigle}</li>
-                  <li>Site web : {props.institution.siteWeb}</li>
-                  <li>Email : {props.institution.email}</li>
-                  <li>Téléphone 1 : {props.institution.telephone1}</li>
-                  <li>Téléphone 2 : {props.institution.telephone1}</li>
-                  <li>Adresse: {props.institution.adresse}</li>
-                  <li>Numéro fiscal: {props.institution.numeroFiscal}</li>
-                  <li>Description : {props.institution.description}</li>
-                  <li>Créée le : {props.institution.createdDate}</li>
-                  <li>Créée par : <a href="#">{props.institution.createdBy}</a></li>
-                  <li>Dernière modification : {props.institution.lastModifiedDate}</li>
-                  <li>Modifiée par: <a href="#">{props.institution.lastModifiedBy}</a></li>
-                </ul>
-              </CCardBody>
-            </CCard>
-          </CCol>
-        </CRow>
-      )
-    }
-    
-    export default Essai
+    export default typeEssai
