13809f548c977e97fea5e627ffcd23aa6689146b
Barbara <bgtheodore18@gmail.com>
2021-05-17 13:14:02 -0400

    Frontend essai

--- geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/FichierController.java
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/FichierController.java
@@ -1,5 +1,5 @@
 
-CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/fichiers")
 public class FichierController {
