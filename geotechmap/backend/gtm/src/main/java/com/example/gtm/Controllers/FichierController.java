4ac04ad6702aa0a13d1e5213d81fbb27292b7368
Barbara <bgtheodore18@gmail.com>
2021-05-25 09:53:54 -0400

    Upload file

--- geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/FichierController.java
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/FichierController.java
@@ -63,24 +63,3 @@
 //     }
     
 // }
-
-@Controller
-@RequestMapping(path = "/files")
-public class FileController {
-
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity  handleFileUpload(@RequestParam("file") MultipartFile file) {
-        try {
-            System.out.printf("File name=%s, size=%s\n", file.getOriginalFilename(),file.getSize());
-            //creating a new file in some local directory
-            File fileToSave = new File("C:\\test\\" + file.getOriginalFilename());
-            //copy file content from received file to new local file
-            file.transferTo(fileToSave);
-        } catch (IOException ioe) {
-            //if something went bad, we need to inform client about it
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-        //everything was OK, return HTTP OK status (200) to the client
-        return ResponseEntity.ok().build();
-    }
-}
---
+++ geotechmap/backend/gtm/src/main/java/com/example/gtm/Controllers/TelechargementFichierController.java
@@ -0,0 +1,27 @@
+package com.example.gtm.Controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import com.example.gtm.Services.FichierService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TelechargementFichierController {
+    
+}
